92737f7ac745ab710788b9d2666d206bb2450c5f
Gavrilik <oleggavrilik@gmail.com>
2022-04-11 13:12:07 +0300

    connect_db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './features/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRESS_HOST,
+      port: Number(process.env.POSTGRESS_PORT), // преобразование в число тк с файла достаем строку
+      username: process.env.POSTGRESS_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRESS_DB,
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
